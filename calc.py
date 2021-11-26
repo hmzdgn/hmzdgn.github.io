4ca0a06fa9b366a2f9dd2bab0b22a4134ca47086
MrR3B00T <48889817+hmzdgn@users.noreply.github.com>
2021-11-26 11:03:22 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,13 @@
+while True:
+	a= int(input("1. sayiyi giriniz: "))
+	b= int(input("2. sayiyi giriniz: "))
+	c= input("1> +     2> -  ")
+	if c=="2":
+		s=a-b
+	else:
+		s=a+b
+	if s==31:
+		print("sjsjsjsjsjsjsjsjsj")
+	else:
+		print(s)
+	print("\n-*-*-*-*-\n")
